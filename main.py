b950ff94d500083d094f40efbf898e39d8272930
chivunira <emmanuelchivunira@gmail.com>
2024-03-19 16:26:29 +0300

    add send email functionality to the app

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import requests
+import send_email
+import os
+from dotenv import load_dotenv
 
-apiKey = '07006c479c204bdab838fcf40c2ff26a'
-url  = ("https://newsapi.org/v2/everything?q=tesla&from=2024-02-19&"
-        "sortBy=publishedAt&"
-        f"apiKey={apiKey}")
+load_dotenv()
+apiKey = os.getenv("apiKey")
 
+url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-02-19&"
+       "sortBy=publishedAt&"
+       f"apiKey={apiKey}")
+
+# make get request
 request = requests.get(url)
+
+# get the json data from the response
 content = request.json()
 
+article_data = ""
+
+# get article titles and descriptions
 for article in content["articles"]:
-        print(article["title"])
-        print(article["description"])
+
+    # check for null values and pass a string in their place
+    title = article["title"] if article["title"] is not None else " "
+    description = article["description"] if article["description"] is not None else " "
+
+    # concatenate the title and description into one data variable
+    articles = str(title) + " : \n " + str(description) + " \n " + article["url"]
+    article_data += articles + " \n \n"
+
+# call the send email function
+send_email.send_email(str(article_data))
+
---
+++ send_email.py
@@ -0,0 +1,23 @@
+import smtplib
+import ssl
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    encoded_message = message.encode('utf-8')
+
+    username = "noreply.myexamm@gmail.com"
+    password = os.getenv("email_password")
+
+    receiver = "chivunira.mbwanga@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, encoded_message)
